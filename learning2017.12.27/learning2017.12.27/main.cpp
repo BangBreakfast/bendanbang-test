909cc55c64a4db08621243215713686463544589
高立人 <gaoliren@gaolirendeMacBook-Pro.local>
2017-12-26 22:42:35 +0800

    bangs

---
+++ learning2017.12.27/learning2017.12.27/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learning2017.12.27
+//
+//  Created by 高立人 on 2017/12/26.
+//  Copyright © 2017年 高立人. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
