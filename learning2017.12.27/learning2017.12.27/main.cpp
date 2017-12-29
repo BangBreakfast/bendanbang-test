769483c0d038747cb26368c5a06f8a8182d2fb7e
高立人 <gaoliren@gaolirendeMacBook-Pro.local>
2017-12-29 10:16:48 +0800

    bangs

--- learning2017.12.27/learning2017.12.27/main.cpp
+++ learning2017.12.27/learning2017.12.27/main.cpp
@@ -2,14 +2,15 @@
 //  main.cpp
 //  learning2017.12.27
 //
-//  Created by 高立人 on 2017/12/26.
-//  Copyright © 2017年 高立人. All rights reserved.
+//  Created by bangzi on 2017/12/26.
+//  Copyright © 2017年 bangzi. All rights reserved.
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
+using namespace std;
+int main() {
     int bangzi=0;
-    std::cout << "Hello, World!\n";
+    cout<<bangzi;
+    cout << "Hello, World!\n";
     return 0;
 }
